6b27d8d48a5cb586dd0e4e2e4af2abe075a44a6f
연민호 <minho.yeon@gmail.com>
2020-02-15 17:16:21 +0900

    1.xml: api로 부터 받은 정보를 text로 read 2.soup: tag로 parsing하기 위해 text를 xml으로 변환 3.for: item에서 listResponseParameter를 받아 출력하기

--- selfmark.py
+++ selfmark.py
@@ -1,9 +1,9 @@
 import urllib.request
 import urllib.parse
-
+from bs4 import BeautifulSoup
 
 api = "http://plus.kipris.or.kr/kipo-api/kipi/trademarkInfoSearchService/getWordSearch"
-service_key = "서비스키"
+service_key = '서비스키'
 query = {
     "searchString": "solev",
     "searchRecentYear": "0"
@@ -13,7 +13,30 @@ query = {
 params = urllib.parse.urlencode(query, doseq=False, safe=' ', encoding=None, errors=None)
 url = api + "?" + params + "&ServiceKey=" + service_key
 
-response = urllib.request.urlopen(url)
-data = response.read()
-print(data.decode("utf-8"))
+request = urllib.request.urlopen(url)
+xml = request.read()
+
+# instance 생성
+soup = BeautifulSoup(xml, 'html.parser')
+
+items = soup.select("item")
+for item in items[:1]:
+    listResponseParameter = [
+    "agentname", "appreferencenumber", "applicantname", "applicationdate", "applicationnumber",\
+    "applicationstatus", "bigdrawing", "classificationcode", "drawing", "fulltext", "indexno",\
+    "internationalregisterdate", "internationalregisternumber", "prioritydate", "prioritynumber",\
+    "publicationdate", "publicationnumber", "regprivilegename", "regreferencenumber", "registrationdate",\
+    "registrationnumber", "registrationpublicdate", "registrationpublicnumber", "title", "viennacode"
+    ]
+    for i in range(len(listResponseParameter)):
+        responseParameter = listResponseParameter[i]
+        print("%s : " % (responseParameter), soup.select_one(responseParameter).text)
+
+
+
+
+
+
+
+
 

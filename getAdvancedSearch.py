5c2c1aaf0005398b214a27a5ae88f3b9c8ccf8de
연민호 <minho.yeon@gmail.com>
2020-02-15 21:55:08 +0900

    전체검2-xml의 태그를 얻어서, 그 태그를 이용해서 출력인자로 사용함

--- getAdvancedSearch.py
+++ getAdvancedSearch.py
@@ -5,24 +5,47 @@ from bs4 import BeautifulSoup
 api = "http://plus.kipris.or.kr/kipo-api/kipi/trademarkInfoSearchService/getAdvancedSearch"
 service_key = "w/X8NRxWUugn=/M/FgwDHmkDKaIKY6WZuERI8aDqT1Y="
 query = {
-    "trademarkName" : "solev",
-    "application": "true",
-    "refused": "true",
-    "expiration": "true",
-    "withdrawal": "true",
-    "publication": "true",
-    "cancel": "true",
-    "abandonment": "true",
-    "trademark": "true",
+    "trademarkName" : "",
+    "classification" : "",
+    "asignProduct" : "",
+    "applicationNumber" : "4120060014133",
+    "registerNumber" : "",
+    "publicationNumber" : "",
+    "registrationPublicNumber" : "",
+    "internationalRegisterNumber" : "",
+    "priorityNumber" : "",
+    "applicationDate" : "",
+    "registerDate" : "",
+    "publicationDate" : "",
+    "registrationPublicDate" : "",
+    "priorityDate" : "",
+    "internationalRegisterDate" : "",
+    "applicantName" : "",
+    "agentName" : "",
+    "viennaCode" : "",
+    "regPrivilegeName" : "",
+    "freeSearch" : "",
+    "similarityCode" : "",
+    "application" : "true",
+    "registration" : "true",
+    "refused" : "true",
+    "expiration" : "true",
+    "withdrawal" : "true",
+    "publication" : "true",
+    "cancel" : "true",
+    "abandonment" : "true",
+    "trademark" : "true",
     "serviceMark" : "true",
     "trademarkServiceMark" : "true",
-    "businessEmblem": "true",
-    "collectiveMark": "true",
-    "internationalMark": "true",
+    "businessEmblem" : "true",
+    "collectiveMark" : "true",
+    "internationalMark" : "true",
     "character" : "true",
     "figure" : "true",
     "compositionCharacter" : "true",
-    "figureComposition" : "true"
+    "figureComposition" : "true",
+    "numOfRows" : "20",
+    "pageNo" : "1"
 }
 
 # values 안에 한글이 있으므로 기계가 아는 문자(숫자, 알파벳)으로 인코딩
@@ -30,28 +53,23 @@ params = urllib.parse.urlencode(query, doseq=False, safe=' ', encoding=None, err
 url = api + "?" + params + "&ServiceKey=" + service_key
 request = urllib.request.urlopen(url)
 
-# url = "http://plus.kipris.or.kr/kipo-api/kipi/trademarkInfoSearchService/getAdvancedSearch?trademarkName=solev&applicantName=&application=true&refused=true&expiration=true&withdrawal=true&publication=true&cancel=true&abandonment=true&trademark=true&serviceMark=true&trademarkServiceMark=true&businessEmblem=true&collectiveMark=true&internationalMark=true&character=true&figure=true&compositionCharacter=true&figureComposition=true&ServiceKey=w/X8NRxWUugn=/M/FgwDHmkDKaIKY6WZuERI8aDqT1Y="
-# request = urllib.request.urlopen(url)
-
 # text로 읽어들임
 xml = request.read()
-# print("xml: ", xml)
 
 # text를 태깅 가능한 xml으로 변환
 soup = BeautifulSoup(xml, 'html.parser')
-# print("soup: ", soup)
+
+# 응답파라미터리스트
+item = soup.find("item")
+listResponseParameter = []
+for tag in item.findChildren():
+    listResponseParameter.append(tag.name)
+# print(listResponseParameter)
 
 # item이 한 사건을 의미함
 items = soup.select("item")
 for item in items[:1]:
-    # 응답파라미터
-    listResponseParameter = [
-    "agentname", "appreferencenumber", "applicantname", "applicationdate", "applicationnumber",\
-    "applicationstatus", "bigdrawing", "classificationcode", "drawing", "fulltext", "indexno",\
-    "internationalregisterdate", "internationalregisternumber", "prioritydate", "prioritynumber",\
-    "publicationdate", "publicationnumber", "regprivilegename", "regreferencenumber", "registrationdate",\
-    "registrationnumber", "registrationpublicdate", "registrationpublicnumber", "title", "viennacode"
-    ]
+    # print(item)
     # 응답파라미터를 받아서 하나씩 출력하기
     for i in range(len(listResponseParameter)):
         responseParameter = listResponseParameter[i]

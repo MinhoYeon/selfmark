a4c938425ba3a8ce4f0bb38b5f1b1f274152f6e3
연민호 <minho.yeon@gmail.com>
2020-02-27 01:50:32 +0900

    상표 검색시 나오는 화면

--- gui_ui_getAdvancedSearch_v2.py
+++ gui_ui_getAdvancedSearch_v2.py
@@ -21,6 +21,7 @@ class MyWindow(QWidget, form_class):
         self.btn_search.clicked.connect(self.mark_search)
 
 
+
     def mark_search(self):
           # le_trademarkName = self.le_trademarkName.text()
         # le_similarityCode = self.le_similarityCode.text()
@@ -78,42 +79,51 @@ class MyWindow(QWidget, form_class):
         soup = BeautifulSoup(xml, 'html.parser')
         items = soup.select("item")
         for item in items[:1]:
+
+            # one mark layout
+            img_Label = QLabel("")
+            indexno_Label = QLabel("")
+            title_Label = QLabel("")
+            applicantName_Label = QLabel("")
+            applicationstatus_Label = QLabel("")
+            applicationnumber_Label = QLabel("")
+            applicationDate_Label = QLabel("")
+
+            left_vbox = QVBoxLayout()
+            left_vbox.addWidget(img_Label)
+            middle_vbox = QVBoxLayout()
+            middle_vbox.addWidget(indexno_Label)
+            middle_vbox.addWidget(QLabel('출원인'))
+            middle_vbox.addWidget(QLabel('상태'))
+            middle_vbox.addWidget(QLabel('출원번호'))
+            middle_vbox.addWidget(QLabel('출원일'))
+            right_vbox = QVBoxLayout()
+            right_vbox.addWidget(title_Label)
+            right_vbox.addWidget(applicantName_Label)
+            right_vbox.addWidget(applicationstatus_Label)
+            right_vbox.addWidget(applicationnumber_Label)
+            right_vbox.addWidget(applicationDate_Label)
+            hbox = QHBoxLayout()
+            hbox.addLayout(left_vbox)
+            hbox.addLayout(middle_vbox)
+            hbox.addLayout(right_vbox)
+
+            self.groupBox.setLayout(hbox)
+
             # Web에서 Image 정보 로드, 웹에서 Load한 Image를 이용하여 QPixmap에 사진데이터를 Load하고, Label을 이용하여 화면에 표시
             urlString = item.select_one("drawing").text
             imageFromWeb = urllib.request.urlopen(urlString).read()
             qPixmapWebVar = QPixmap()
             qPixmapWebVar.loadFromData(imageFromWeb)
-            img_Label = QLabel()
             img_Label.setPixmap(qPixmapWebVar)
-
-            indexno_Label = QLabel()
+            # 다른정보들
             indexno_Label.setText(item.select_one("indexno").text)
-
-            title_Label = QLabel()
             title_Label.setText(item.select_one("title").text)
-
-            applicationstatus_Label = QLabel()
-            applicationstatus_Label.setText(item.select_one("applicationstatus").text)
-
-            applicationnumber_Label = QLabel()
-            applicationnumber_Label.setText(item.select_one("applicationnumber").text)
-
-            applicantName_Label = QLabel()
             applicantName_Label.setText(item.select_one("applicantName").text)
-
-            applicationDate_Label = QLabel()
+            applicationstatus_Label.setText(item.select_one("applicationstatus").text)
+            applicationnumber_Label.setText(item.select_one("applicationnumber").text)
             applicationDate_Label.setText(item.select_one("applicationDate").text)
 
-
-
-
-            vbox = QVBoxLayout()
-            vbox.addWidget(img_Label)
-            vbox.addWidget(indexno_Label)
-            vbox.addWidget(title_Label)
-            vbox.addWidget(applicationstatus_Label)
-            vbox.addWidget(applicationnumber_Label)
-            self.groupBox.setLayout(vbox)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
